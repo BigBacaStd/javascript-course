612b94ef921d733937d4ada23fe7228b0cc70fc1
PoloBaca <thebigbaca@gmail.com>
2024-12-07 18:36:07 -0600

    commit Dec 7 2024

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,10 @@
+// Declaración de una variable de tipo número con valor 42.
+
+/*Este es un
+* comentario de
+* varias lineas
+* para probar esta onda*/
+
+let numero = 42; // aqui se le asigna el valor 42 a la variable.
+
+console.log(numero);
