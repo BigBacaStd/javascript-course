e9acf27682259738b96ecbc12148311311e7b108
PoloBaca <thebigbaca@gmail.com>
2024-12-09 22:44:44 -0600

    commit Dec 9 2024

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,13 @@
+let animales = ['gato', 'perro']
+
+console.log(animales);
+console.log(animales[1]);
+
+animales[2] = 'perico';
+console.log(animales);
+
+animales[9] = 'ganzo';
+console.log(animales[7]);
+
+console.log(typeof animales);
+console.log(animales.length);
