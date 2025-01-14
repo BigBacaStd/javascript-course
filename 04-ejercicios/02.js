e609bd1853d80d4bc7b9e815c34ca3bde9f95edf
PoloBaca <thebigbaca@gmail.com>
2025-01-13 20:16:13 -0600

    commit January 13th 2025

--- 04-ejercicios/02.js
+++ 04-ejercicios/02.js
@@ -3,7 +3,7 @@
 Nombre: Ancho x Alto
 8K 7680 x 4320
 4K 3840 x 2160
-WQHD 1366 x 768
+WQHD 2560 x 1440
 FHD 1920 x 1080
 HD 1366 x 768
 
@@ -12,7 +12,18 @@ HD 1366 x 768
 
 function nombreResolucion(ancho, alto) {
 
+    if (ancho >= 7680 && alto >= 4320) {
+        return '8K';
+    }else if (ancho >= 3840 && alto >= 2160) {
+        return '4K';
+    }else if (ancho >= 1366 && alto >= 768) {
+        return 'WQHD';
+    }else if (ancho >= 1920 && alto >= 1080) {
+        return 'FHD';
+    }else if (ancho >= 1366 && alto >= 768) {
+        return 'HD';
+    }
 }
 
-let nombre = nombreResolucion(1366, 768);
+let nombre = nombreResolucion(2560, 2160);
 console.log(nombre); // 
