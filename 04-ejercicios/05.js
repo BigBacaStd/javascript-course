5cd73f2128521ae5fce50eede193f5b1b6778bae
PoloBaca <thebigbaca@gmail.com>
2025-01-19 23:11:51 -0600

    commit January 19 2025

---
+++ 04-ejercicios/05.js
@@ -0,0 +1,23 @@
+/*
+*
+* Crear un algoritmo que devuelva el número menor
+* y valo de un array de números.
+*
+* */
+
+let array = [2, 5, 7, 15, -5, -100, -55];
+
+function getMenorMayor(arr) {
+
+    let menor = arr[0];
+    let mayor = arr[0];
+
+    for (numero of arr) {
+        menor = menor < numero? menor : numero;
+        mayor = mayor > numero? mayor : numero;
+    }
+    return {menor, mayor};
+}
+
+let numeros = getMenorMayor(array);
+console.log(numeros)
